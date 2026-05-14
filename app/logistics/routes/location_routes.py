c5d67e3fd3e6ea76c3d7925ae0aeaf85ed62decf
Lemin <lenin28026@gmail.com>
2026-05-14 19:38:28 -0400

    Conflictos de fusión resueltos con el módulo de sedes

--- app/logistics/routes/location_routes.py
+++ app/logistics/routes/location_routes.py
@@ -2,20 +2,18 @@ from flask import Blueprint, render_template, redirect, url_for, flash
 from app.logistics.requests.location_request import LocationForm
 from app.logistics.services.register_location import register_location_service
 
-# Definimos el Blueprint
-location_bp = Blueprint('logistics', __name__)
+location_bp = Blueprint('location_routes_bp', __name__)
 
 @location_bp.route('/locations', methods=['GET', 'POST'])
 def register_location():
     form = LocationForm()
     
     if form.validate_on_submit():
-        # Llamamos a tu lógica de servicio que ya programaste
         success, message = register_location_service(form)
         
         if success:
             flash(message, "success")
-            return redirect(url_for('logistics.register_location'))
+            return redirect(url_for('location_routes_bp.register_location'))
         else:
             flash(message, "danger")
             
