a5f42b0ce7164b5a0d10da1ce479826d03b971e6
diegochirinos-cyber <diegoch0505@gmail.com>
2026-05-25 19:43:54 -0400

    Fix: Listado de Estados para Sedes Actualizado

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -2,10 +2,28 @@
 
 VENEZUELA_STATES = [
     ('', 'Seleccione un Estado...'),
+    ('Amazonas', 'Amazonas'),
+    ('Anzoátegui', 'Anzoátegui'),
+    ('Apure', 'Apure'),
     ('Aragua', 'Aragua'),
+    ('Barinas', 'Barinas'),
+    ('Bolívar', 'Bolívar'),
     ('Carabobo', 'Carabobo'),
+    ('Cojedes', 'Cojedes'),
+    ('Delta Amacuro', 'Delta Amacuro'),
     ('Distrito Capital', 'Distrito Capital'),
+    ('Falcón', 'Falcón'),
+    ('Guárico', 'Guárico'),
     ('Lara', 'Lara'),
+    ('Mérida', 'Mérida'),
     ('Miranda', 'Miranda'),
+    ('Monagas', 'Monagas'),
+    ('Nueva Esparta', 'Nueva Esparta'),
+    ('Portuguesa', 'Portuguesa'),
+    ('Sucre', 'Sucre'),
+    ('Táchira', 'Táchira'),
+    ('Trujillo', 'Trujillo'),
+    ('Vargas', 'Vargas (La Guaira)'),
+    ('Yaracuy', 'Yaracuy'),
     ('Zulia', 'Zulia')
 ]
