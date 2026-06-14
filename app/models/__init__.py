343f28f0e2324fcbec16883040d9c073c9de8ea1
Lemin <lenin28026@gmail.com>
2026-06-14 16:28:32 -0400

    arreglo de init y nueva carpeta

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,6 @@ from app.extensions import db
 
 # Importamos todo para que Alembic (Migrate) lo detecte
 from .inventory_model import Product, Inventory, Category
-from .logistics_model import Location, Supplier, Purchase, PurchaseDetail, Movement, MovementDetail
+from .logistics_model import Location, Supplier, Purchase, PurchaseDetail, Movement, MovementDetail, ExchangeRateHistory
 from .security_model import Role, User, Notification, PasswordRecovery, LoginAudit, user_locations 
 from .waste_model import WasteType, Waste, AuditLog
