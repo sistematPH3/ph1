ed488719021d6630c0ca080de293cc849ccf90af
diegochirinos-cyber <diegoch0505@gmail.com>
2026-07-03 17:57:58 -0400

    Tabla Product_types declarada en el archivo init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.extensions import db
 
-from .inventory_model import Product, Inventory, Category
+
+from .inventory_model import Product, Inventory, Category, ProductType
 from .logistics_model import Location, Supplier, Purchase, PurchaseDetail, Movement, MovementDetail, ExchangeRateHistory, PurchaseAuditLog
 from .security_model import Role, User, Notification, PasswordRecovery, LoginAudit, user_locations 
 from .waste_model import WasteType, Waste, AuditLog
